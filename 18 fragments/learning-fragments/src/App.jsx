b4b922d5244a2b4c8ff706e0bb13e14af29a1425
SharafAi <sharafhassan33@gmail.com>
2025-11-03 09:36:33 +0500

    ternary operator method 2

--- 18 fragments/learning-fragments/src/App.jsx
+++ 18 fragments/learning-fragments/src/App.jsx
@@ -3,10 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   let FoodItems = ["Milk", "Fish", "Orange", "Meat", "Banana"];
+
+  let EmptyMsg = FoodItems.length === 0 ? <h3>im still hungry</h3> : null;
   return (
     <>
       <h1>Healthy Foods</h1>
-      {FoodItems.length === 0 ? <h3>im still hungry</h3> : null}
+      {EmptyMsg}
       <ul className="list-group">
         {FoodItems.map((item) => (
           <li key={item} className="list-group-item">
