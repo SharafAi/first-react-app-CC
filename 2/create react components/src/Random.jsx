cda1d872dabd79cd7e79943775329e809d21e005
SharafAi <sharafhassan33@gmail.com>
2025-11-03 09:56:04 +0500

    some parts of this folder was not saved

--- 2/create react components/src/Random.jsx
+++ 2/create react components/src/Random.jsx
@@ -1,13 +1,16 @@
 function Random() {
   let number = Math.random() * 100;
 
-  return <h1 style={{
-    backgroundColor: 'red',
-    borderRadius:'5',
-  }}>
-    Random number is: {Math.round(number)} 
-  </h1>
-  
+  return (
+    <h1
+      style={{
+        backgroundColor: "red",
+        borderRadius: "20px",
+      }}
+    >
+      Random number is: {Math.round(number)}
+    </h1>
+  );
 }
 
 export default Random;
