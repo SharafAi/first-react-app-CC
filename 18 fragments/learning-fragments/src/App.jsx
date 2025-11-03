8d8bc195475564e2e90f27bcaf7b1167971ea87f
SharafAi <sharafhassan33@gmail.com>
2025-11-03 09:29:11 +0500

    using if else statements

--- 18 fragments/learning-fragments/src/App.jsx
+++ 18 fragments/learning-fragments/src/App.jsx
@@ -3,6 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   let FoodItems = ["Milk", "Fish", "Orange", "Meat", "Banana"];
+
+  if (FoodItems.length === 0){
+    return <h3>im still hungry</h3>
+  }
   return (
     <>
       <h1>Healthy Foods</h1>
