65a8359a124c2e7f66363d09f059470d5b91ebc2
SharafAi <sharafhassan33@gmail.com>
2025-11-03 09:43:44 +0500

    using logical operators

--- 18 fragments/learning-fragments/src/App.jsx
+++ 18 fragments/learning-fragments/src/App.jsx
@@ -4,11 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   let FoodItems = ["Milk", "Fish", "Orange", "Meat", "Banana"];
 
-  let EmptyMsg = FoodItems.length === 0 ? <h3>im still hungry</h3> : null;
   return (
     <>
       <h1>Healthy Foods</h1>
-      {EmptyMsg}
+      {FoodItems.length === 0 && <h3>im still hungry</h3>}
       <ul className="list-group">
         {FoodItems.map((item) => (
           <li key={item} className="list-group-item">
