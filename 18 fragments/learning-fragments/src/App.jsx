cb84bb35bfa862d759598df5425c65933bf3b439
SharafAi <sharafhassan33@gmail.com>
2025-11-03 07:59:16 +0500

    learning fragments

---
+++ 18 fragments/learning-fragments/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return ( 
+    <> 
+      <h1>Healthy Foods</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Milk</li>
+        <li class="list-group-item">Banana</li>
+        <li class="list-group-item">Meat</li>
+        <li class="list-group-item">Orange</li>
+        <li class="list-group-item">Fish</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
