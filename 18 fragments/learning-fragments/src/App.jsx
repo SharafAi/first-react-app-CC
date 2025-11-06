da82db99c8885edb906eb9056ac3a20b91dfe11d
SharafAi <sharafhassan33@gmail.com>
2025-11-06 17:21:59 +0500

    passing children to a container component

--- 18 fragments/learning-fragments/src/App.jsx
+++ 18 fragments/learning-fragments/src/App.jsx
@@ -1,16 +1,26 @@
 import FoodItems from "./components/FoodItems";
-import "bootstrap/dist/css/bootstrap.min.css";
 import ErrorMsg from "./components/ErrorMsg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "./components/Container";
 
 function App() {
   let foodList = ["Milk", "Fish", "Orange", "Meat", "Banana"];
 
   return (
     <>
-      <h1 className="Item-Heading">Healthy Foods</h1>
-      <ErrorMsg items={foodList} />
-      <FoodItems items={foodList} />
+      <Container>
+        <h1 className="Item-Heading">Healthy Foods</h1>
+        <ErrorMsg items={foodList} />
+        <FoodItems items={foodList} />
+      </Container>
+
+      <Container>
+        <p>
+          above is the healthy foods that are good for your health and well
+          being
+        </p>
+      </Container>
     </>
   );
 }
--- 18 fragments/learning-fragments/src/components/Item.jsx
+++ 18 fragments/learning-fragments/src/components/Item.jsx
@@ -1,8 +1,12 @@
 import styles from "./Item.module.css";
 
 const Item = ({ foodList }) => {
-  console.log(styles) 
-  return <li className={`${styles['Bg-Item']}`}><span className={styles["Item-span"]}>{foodList}</span></li>;
+  console.log(styles);
+  return (
+    <li className={`${styles["Bg-Item"]}`}>
+      <span className={styles["Item-span"]}>{foodList}</span>
+    </li>
+  );
 };
 
 export default Item;
