06f13bd699b5ae2fd1bfd9fb256c4fdadc8dace1
SharafAi <sharafhassan33@gmail.com>
2025-11-07 17:24:50 +0500

    handling events

--- 18 fragments/learning-fragments/src/App.jsx
+++ 18 fragments/learning-fragments/src/App.jsx
@@ -3,6 +3,7 @@ import ErrorMsg from "./components/ErrorMsg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
 
 function App() {
   let foodList = ["Milk", "Fish", "Orange", "Meat", "Banana"];
@@ -12,15 +13,16 @@ function App() {
       <Container>
         <h1 className="Item-Heading">Healthy Foods</h1>
         <ErrorMsg items={foodList} />
+        <FoodInput />
         <FoodItems items={foodList} />
       </Container>
 
-      <Container>
+      {/* <Container>
         <p>
           above is the healthy foods that are good for your health and well
           being
         </p>
-      </Container>
+      </Container> */}
     </>
   );
 }
--- 18 fragments/learning-fragments/src/components/Item.jsx
+++ 18 fragments/learning-fragments/src/components/Item.jsx
@@ -1,10 +1,16 @@
 import styles from "./Item.module.css";
 
 const Item = ({ foodList }) => {
-  console.log(styles);
+  const buyButtonClicked = (event) => {
+    console.log(event);
+    console.log(`you bought ${foodList} succesfully`);
+  };
   return (
     <li className={`${styles["Bg-Item"]}`}>
       <span className={styles["Item-span"]}>{foodList}</span>
+      <button className={styles.buyButton} onClick={(event) => buyButtonClicked(event)}>
+        buy
+      </button>
     </li>
   );
 };
